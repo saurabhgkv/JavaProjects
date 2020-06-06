f5b2f1df72db797781d90f5d55e4b10419cdcb23
Saurabh Kumar <darshil2009@gmail.com>
2020-06-06 02:34:56 +0200

    Added parameters

--- DBProject/src/connection/ConnectionClass.java
+++ DBProject/src/connection/ConnectionClass.java
@@ -1,12 +1,17 @@
 package connection;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 
 public class ConnectionClass {
 	
@@ -14,6 +19,8 @@ public class ConnectionClass {
 	String username;
 	String password;
 	Connection con;
+	File file = new File(System.getProperty("user.dir")+"/log.txt");
+	BufferedWriter  output = null;
 	public ConnectionClass(String connDetails, String username, String password) {
 		//super();
 		this.connDetails = connDetails;
@@ -24,16 +31,24 @@ public class ConnectionClass {
     public void makeConnection()
     {
     	try {
-			con = DriverManager.getConnection
-					("jdbc:oracle:thin:@localhost:1521:xe","hr","password23");
+    		con = DriverManager.getConnection
+("jdbc:oracle:thin:@localhost:1521:xe",System.getProperty("system.UserId")
+		,System.getProperty("system.Password"));
+    		//("jdbc:oracle:thin:@localhost:1521:xe","hr","password23#@");
 		} catch (SQLException e) {
-			e.printStackTrace();
+			e.printStackTrace(); System.getProperty("system.UserId");
 		}
     	
     }
     
     public void runQuery(){
     	Statement stmt = null;
+    	try {
+			output = new BufferedWriter(new FileWriter(file));
+		} catch (IOException e4) {
+			// TODO Auto-generated catch block
+			e4.printStackTrace();
+		}
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException e3) {
@@ -50,9 +65,17 @@ public class ConnectionClass {
 		}
     	try {
     		System.out.println("Number of records - "+rs.getFetchSize());
-    		JOptionPane.showMessageDialog(null,"Number of records - "+rs.getFetchSize());
-			while(rs.next())
-			JOptionPane.showMessageDialog(null,rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+    		//JOptionPane.showMessageDialog(null,"Number of records - "+rs.getFetchSize());
+			while(rs.next()){
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+			try {
+				output.write("Hi");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			}
+			//JOptionPane.showMessageDialog(null,rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
--- DBProject/src/connection/MasterClass.java
+++ DBProject/src/connection/MasterClass.java
@@ -1,8 +1,10 @@
 package connection;
 
+import java.io.IOException;
+
 public class MasterClass{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		ConnectionClass cc = new ConnectionClass("BAC","HAT","CAT");
 		cc.makeConnection();
